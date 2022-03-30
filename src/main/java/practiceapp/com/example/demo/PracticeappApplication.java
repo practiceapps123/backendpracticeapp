dac865f553b3c63060c0db0b8e709866659f7aaf
Pooja Dutt <pdutt@wisc.edu>
2022-03-29 19:00:05 -0500

    new application

---
+++ src/main/java/practiceapp/com/example/demo/PracticeappApplication.java
@@ -0,0 +1,13 @@
+package practiceapp.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practiceapp/com/example/demo/PracticeappApplicationTests.java
@@ -0,0 +1,13 @@
+package practiceapp.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
