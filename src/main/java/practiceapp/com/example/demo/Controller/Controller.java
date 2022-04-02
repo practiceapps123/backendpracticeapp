accf0df41d69eaddeff0da38e0957b7436c02c1f
Pooja Dutt <pdutt@wisc.edu>
2022-04-01 22:33:46 -0500

    Added new files

---
+++ src/main/java/practiceapp/com/example/demo/Controller/Controller.java
@@ -0,0 +1,16 @@
+package practiceapp.com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/health")
+    public String getHealthCheck() {
+        return "Yay! I hit my endpoint!";
+    }
+
+}
